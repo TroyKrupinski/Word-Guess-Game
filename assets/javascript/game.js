0d580f61b2ff70e86671ab291c5abc101dc716ec
Troy Krupinski <43382791+TroyKrupinski@users.noreply.github.com>
2018-10-23 17:57:29 -0500

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,11 @@
-var Song1 = ["R", "e", "s", "o", "n", "a", "n", "c", "e"];
-var Song2;
+
 var Song3;
 var Song4;
 var Song5;
 var guesses = 6;
 var guesslist = [];
 var RandomSong;
-songlist = ["Resonance"];
+songlist = ["Resonance","Crisis"];
 Alth = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
 play2 = ["Resonance.mp4","Crisis.mp4"];
 isplaying = false;
@@ -16,13 +15,23 @@ function Reset(){
     audioElement.play();
     console.log(RandomSong);
 }
-function pplay(){
-    
+function pplay(RandomSong){
+    var Song1 = ["r", "e", "s", "o", "n", "a", "n", "c", "e"];
+    var Song2 = ["c", "r", "i", "s", "i","s"];
+    var song = ""; 
+    console.log(RandomSong);
     console.log(Alth);
     if(isplaying == false){
         audioElement = document.createElement('audio');
         RandomSong = play2[Math.floor(Math.random()*play2.length)];
+        if(RandomSong == "Resonance.mp4"){
+            song = songlist[0];
+        }
+        else{
+            song = songlist[1];
+        }
         console.log(RandomSong);
+        console.log(song);
         audioElement.innerHTML = '<source src="' + 'assets/Songs/' + RandomSong + '" type="audio/mpeg" />';  
         audioElement.play();
         
@@ -32,14 +41,15 @@ function pplay(){
         
         console.log(isplaying);
         isplaying = true;
-        if(RandomSong == "Resonance.mp4"){
+        
 
             letters = [];
-           
+            playing = [];
             table = [];
             table2 = [];
+            exclude = [];
             tableprint = table.join(" ");
-            for(s = 0; s<Song1.length; s++){
+            for(s = 0; s<song.length; s++){
                 
                 table[s] = "_";
             }
@@ -55,78 +65,61 @@ function pplay(){
                 s = s.toLowerCase();
                 console.log(x);
                 compare = table.toString();
-                compare2 = Song1.toString();
-            
-                    if(x == 65){
+                
+                if(RandomSong == "Resonance.mp4"){
+                    playing = Song1;
+                    compare2 = Song1.toString();
+                }
+                else if(RandomSong == "Crisis.mp4"){
+                    playing = Song2;
+                    compare2 = Song2.toString();
+                }
+            for(i = 0; i<song.length; i++){
+                console.log("t");
+                console.log(s);
+                console.log(song.length);
+                console.log(Song1[i]);
+                    if(s == playing[i]){
                         console.log("test");
                         table.push
-                        table[5] = "a";
+                        table[i] = s;
                         guesslist.push(s);
                         console.log(guesslist);
                         document.getElementById("ttt").innerHTML = "<h2>Guess:" + table + "</h2>";
-                    }
-                    else if(x == 82){
-                        console.log("test");
-                        table.push
-                        table[0] = "R";
                         
-                        
-                        document.getElementById("ttt").innerHTML = "<h2>Guess:" + table + "</h2>";
+              
                     }
-                    else if(x == 69){
-                        console.log("test");
-                        table.push
-                        table[1] = "e";
-                        table[8] = "e";
-                        document.getElementById("ttt").innerHTML = "<h2>Guess:" + table + "</h2>";
-                    }
-                    else if(x == 79){
-                        console.log("test");
-                        table.push
-                        table[3] = "o";
-                        
-                        document.getElementById("ttt").innerHTML = "<h2>Guess:" + table + "</h2>";
-                    }
-                    else if(x == 83){
-                        console.log("test");
-                        table.push
-                        table[2] = "s";
-                        
-                        document.getElementById("ttt").innerHTML = "<h2>Guess:" + table + "</h2>";
-                    }
-                    else if(x == 78){
-                        console.log("test");
-                        table.push
-                        table[4] = "n";
-                        table[6] = "n";
-                        
+               else{
 
-                        document.getElementById("ttt").innerHTML = "<h2>Guess:" + table + "</h2>";
-                    }
-                    else if(x == 67){
-                        console.log("test");
-                        table.push
-                        table[7] = "c";
-                        console.log(table);
-
-                        document.getElementById("ttt").innerHTML = "<h2>Guess:" + table + "</h2>";
-                    }
-
+               }
+            }
                     
-                    else{
-                        for(i = 0; i < letters.length; i++){
+                    
+
+                     
+                        for(j = 0; j < letters.length; j++){    
                             console.log(s);
-                            if(s == letters[i]){
+                            
+                            if(s == letters[j]){
                                 check = true;
                                 console.log("check");
                                 
+                                
                             }
-
+                            
                         }
+                    for(p = 0; p < playing.length; p++){
+                        if(s == playing[p]){
+                            check = true;
+                        }
+                    }
+                    
                         if(check == false){
+                            
                             guesses --;
                             letters.push(s);
                             console.log("minus");
+                            
                         }
                         else{
                             
@@ -136,7 +129,10 @@ function pplay(){
                         }
                         document.getElementById("numb").innerHTML = "<h3>Number of Guesses: " + guesses +  "</h3>";
                         document.getElementById("letter").innerHTML = "<h3>Letters tried: " + letters +  "</h3>";
-                    }
+                       
+                    
+                    
+                    
                     if(guesses == 0){
                         audioElement.pause();
                         console.log("pause");
@@ -145,7 +141,15 @@ function pplay(){
                         document.getElementById('te').innerHTML = "<marquee>:(</marquee>";
                         document.getElementById("numb").innerHTML = "";
                         if(x == 13){
+                            RandomSong = play2[Math.floor(Math.random()*play2.length)];
                             console.log("enter");
+                            guesses = 6;
+                            isplaying = false;    
+                            
+                                             
+                                             
+                            
+                            
                         }
                         
                     }
@@ -157,11 +161,13 @@ function pplay(){
 
                         document.getElementById('te').innerHTML = "<marquee>✌</marquee>";
                         if(x == 13){
-                            console.log("enter");
-                            isplaying = false;
-                           
-                            guesses = 6;
                             RandomSong = play2[Math.floor(Math.random()*play2.length)];
+                            guesses = 6;
+                            isplaying = false;
+                            pplay();
+                                            
+                            
+                            
                            
                         }
                         
@@ -175,12 +181,10 @@ function pplay(){
               document.addEventListener('keydown', Miscari);
 
             console.log("test");
-        }
-        else{
-            console.log(":O");
-        }
+      
         
-    }
+    
+}
 else{
     
     console.log("already playing");
